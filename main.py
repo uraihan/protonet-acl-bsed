00f029636279e2f5d1e5e7623e42069f7629afc2
uraihan <umair.umer26@gmail.com>
2025-07-18 19:15:02 +0300

    Init python environment

---
+++ main.py
@@ -0,0 +1,11 @@
+import argparse
+from args import args
+
+
+def main():
+    print("Hello from fsl-bsed!")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='main')
+    main()
